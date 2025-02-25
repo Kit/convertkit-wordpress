5163642ff59e4876ce231bb1a77825d871c6c476
Tim Carr <tim@n7studios.co.uk>
2025-02-25 09:35:18 +0800

    Updated GPL URLs to v3

--- wp-convertkit.php
+++ wp-convertkit.php
@@ -14,7 +14,7 @@
  * Author URI: https://kit.com/
  * Text Domain: convertkit
  * License:     GPLv3 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
 
 // Bail if Kit is alread loaded.
