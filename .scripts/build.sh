4172b2228ac81c63d17a7ed052a862191cc6c5ee
Tim Carr <tim@n7studios.co.uk>
2025-03-26 19:03:40 +0800

    Exclude files when building ZIP file locally

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -4,6 +4,26 @@ php create-actions-filters-docs.php
 # Generate .pot file
 php -n $(which wp) i18n make-pot ../ ../languages/convertkit.pot
 
-# Build ZIP file
-rm ../convertkit.zip
-cd .. && zip -r convertkit.zip . -x "*.git*" -x ".scripts/*" -x ".wordpress-org/*" -x "tests/*" -x "vendor/*" -x "*.distignore" -x "*.env.*" -x "*codeception.*" -x "composer.json" -x "composer.lock" -x "*.md" -x "log.txt" -x "phpcs.xml" -x "phpstan.neon" -x "phpstan.neon.dist" -x "phpstan.neon.example" -x "*.DS_Store"
+# Build ZIP file, excluding non-Plugin files
+cd ..
+rm convertkit.zip
+zip -r convertkit.zip . \
+-x "*.git*" \
+-x ".devcontainer/*" \
+-x ".scripts/*" \
+-x ".wordpress-org/*" \
+-x "log/*" \
+-x "tests/*" \
+-x "vendor/*" \
+-x "*.distignore" \
+-x "*.env.*" \
+-x ".gitignore" \
+-x "*.md" \
+-x "*.yml" \
+-x "composer.json" \
+-x "composer.lock" \
+-x "*.xml" \
+-x "*.neon" \
+-x "*.dist" \
+-x "*.example" \
+-x "*.DS_Store" \
