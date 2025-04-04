d79fd41f78c1b1fba4cfad9ac3f1ef90afcba7ef
Tim Carr <tim@n7studios.co.uk>
2025-04-04 16:47:02 +0800

    Updated build scripts to include wp dist-archive option

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -4,6 +4,28 @@ php create-actions-filters-docs.php
 # Generate .pot file
 php -n $(which wp) i18n make-pot ../ ../languages/convertkit.pot
 
-# Build ZIP file
-rm ../convertkit.zip
-cd .. && zip -r convertkit.zip . -x "*.devcontainer*" -x "*.git*" -x ".scripts/*" -x ".wordpress-org/*" -x "tests/*" -x "vendor/*" -x "*.distignore" -x "*.env.*" -x "*codeception.*" -x "composer.json" -x "composer.lock" -x "*.md" -x "log.txt" -x "phpcs.xml" -x "phpcs.tests.xml" -x "phpstan-dev.neon" -x "phpstan.neon" -x "phpstan.neon.dist" -x "phpstan.neon.example" -x "*.DS_Store"
+# Build ZIP file, excluding non-Plugin files
+cd ..
+rm convertkit.zip
+zip -r convertkit.zip . \
+-x "*.devcontainer*" \
+-x "*.git*" \
+-x ".scripts/*" \
+-x ".wordpress-org/*" \
+-x "log/*" \
+-x "tests/*" \
+-x "vendor/*" \
+-x "!vendor/convertkit" \
+-x "*.DS_Store" \
+-x "*.distignore" \
+-x "*.env" \
+-x "*.md" \
+-x "*.dist" \
+-x "*.example" \
+-x "*.neon" \
+-x "*.testing" \
+-x "*.xml" \
+-x "*.yml" \
+-x "*.zip" \
+-x "composer.json" \
+-x "composer.lock"
