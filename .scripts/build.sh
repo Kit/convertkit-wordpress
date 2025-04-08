fbca41ea94e22b9fafcd773844e1ce13cd755636
Tim Carr <tim@n7studios.co.uk>
2025-04-08 16:32:08 +0800

    Fix build script to include Kit WordPress Libraries

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -4,8 +4,14 @@ php create-actions-filters-docs.php
 # Generate .pot file
 php -n $(which wp) i18n make-pot ../ ../languages/convertkit.pot
 
-# Build ZIP file, excluding non-Plugin files
+# Remove vendor directory
 cd ..
+rm -rf vendor
+
+# Run composer to only install non-dev dependencies
+composer install --no-dev
+
+# Build ZIP file, excluding non-Plugin files
 rm convertkit.zip
 zip -r convertkit.zip . \
 -x "*.git*" \
@@ -14,7 +20,11 @@ zip -r convertkit.zip . \
 -x ".wordpress-org/*" \
 -x "log/*" \
 -x "tests/*" \
--x "vendor/*" \
+-x "vendor/composer/*" \
+-x "vendor/convertkit/convertkit-wordpress-libraries/.github" \
+-x "vendor/convertkit/convertkit-wordpress-libraries/tests/*" \
+-x "vendor/convertkit/convertkit-wordpress-libraries/composer.json" \
+-x "vendor/autoload.php" \
 -x "*.distignore" \
 -x "*.env.*" \
 -x ".gitignore" \
@@ -27,3 +37,6 @@ zip -r convertkit.zip . \
 -x "*.dist" \
 -x "*.example" \
 -x "*.DS_Store" \
+
+# Run composer to install dev dependencies, returning enviornment back to original state
+composer update
