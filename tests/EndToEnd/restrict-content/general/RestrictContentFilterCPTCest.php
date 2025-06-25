8de5a2268e616aa4c3ba1c7a374e64b1d368803e
Tim Carr <tim@n7studios.co.uk>
2025-06-25 14:01:51 +0800

    Added tests

--- tests/EndToEnd/restrict-content/general/RestrictContentFilterCPTCest.php
+++ tests/EndToEnd/restrict-content/general/RestrictContentFilterCPTCest.php
@@ -243,6 +243,93 @@ class RestrictContentFilterCPTCest
 	}
 
 	/**
+	 * Test that filtering by 'All member-only content' works on the CPT screen.
+	 *
+	 * @since   2.8.3
+	 *
+	 * @param   EndToEndTester $I  Tester.
+	 */
+	public function testFilterByAllMemberOnlyContent(EndToEndTester $I)
+	{
+		// Setup Plugin.
+		$I->setupKitPlugin($I);
+
+		// Create a mix of Posts restricted and not restricted to Forms, Tags and Products.
+		$I->createRestrictedContentPage(
+			$I,
+			[
+				'post_type'                => 'article',
+				'post_title'               => 'Kit: Article: Restricted Content: Form: Filter Test',
+				'restrict_content_setting' => 'form_' . $_ENV['CONVERTKIT_API_FORM_ID'],
+			]
+		);
+		$I->createRestrictedContentPage(
+			$I,
+			[
+				'post_type'                => 'article',
+				'post_title'               => 'Kit: Article: Restricted Content: Tag: Filter Test',
+				'restrict_content_setting' => 'tag_' . $_ENV['CONVERTKIT_API_TAG_ID'],
+			]
+		);
+		$I->createRestrictedContentPage(
+			$I,
+			[
+				'post_type'                => 'article',
+				'post_title'               => 'Kit: Article: Restricted Content: Product: Filter Test',
+				'restrict_content_setting' => 'product_' . $_ENV['CONVERTKIT_API_PRODUCT_ID'],
+			]
+		);
+		$I->havePostInDatabase(
+			[
+				'post_type'  => 'article',
+				'post_title' => 'Kit: Article: Standard',
+				'meta_input' => [
+					'_wp_convertkit_post_meta' => [
+						'form'             => '0',
+						'landing_page'     => '',
+						'tag'              => '',
+						'restrict_content' => '0',
+					],
+				],
+			]
+		);
+		$I->havePostInDatabase(
+			[
+				'post_type'  => 'article',
+				'post_title' => 'Kit: Article: Standard: No Meta',
+			]
+		);
+
+		// Navigate to Articles.
+		$I->amOnAdminPage('edit.php?post_type=article');
+
+		// Wait for the WP_List_Table of Articles to load.
+		$I->waitForElementVisible('tbody#the-list');
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+
+		// Filter by All member-only content.
+		$I->selectOption('#wp-convertkit-restrict-content-filter', 'All member-only content');
+		$I->click('Filter');
+
+		// Wait for the WP_List_Table of Articles to load.
+		$I->waitForElementVisible('tbody#the-list');
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+
+		// Confirm that the Restrict Content Articles are listed.
+		$I->see('Kit: Article: Restricted Content: Form: Filter Test');
+		$I->see('Kit: Article: Restricted Content: Tag: Filter Test');
+		$I->see('Kit: Article: Restricted Content: Product: Filter Test');
+
+		// Confirm that no non-Restrict Content Posts are not listed.
+		$I->dontSee('Kit: Article: Standard');
+		$I->dontSee('Kit: Article: Standard: No Meta');
+	}
+
+	/**
 	 * Deactivate and reset Plugin(s) after each test, if the test passes.
 	 * We don't use _after, as this would provide a screenshot of the Plugin
 	 * deactivation and not the true test error.
