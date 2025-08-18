0a7cc7d346b7d225ffbe38d8ec9a5c1564a39c3b
Tim Carr <tim@n7studios.co.uk>
2025-08-18 15:09:07 +0800

    Coding standards

--- includes/class-convertkit-html-parser.php
+++ includes/class-convertkit-html-parser.php
@@ -33,6 +33,7 @@ class ConvertKit_HTML_Parser {
 	 * @since   3.0.0
 	 *
 	 * @param   string $content HTML content to load.
+	 * @param   int    $flags   DOMDocument flags.
 	 */
 	public function __construct( $content, $flags = false ) {
 
