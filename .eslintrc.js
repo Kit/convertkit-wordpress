1463eeb0b17607ea729bf8523153bff310068c22
Tim Carr <tim@n7studios.co.uk>
2025-09-24 09:53:41 +0800

    Improve JS Rules and Workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,10 +13,15 @@ module.exports = {
 		convertkit_shortcodes: 'readonly',
 	},
 	rules: {
-		// Turn off specific rules
+		// Globals are not camelcase; in the future, we should update JS to meet camelcase standards.
 		camelcase: 'off',
+		// We don't yet manage dependencies, so some files report functions that are not defined, as they're in different files,
+		// despite being enqueued on the same page.
+		// In the future, we will use `wp-scripts build` to build single backend + frontend JS, which will fix this issue.
 		'no-undef': 'off',
+		// If debugging is enabled in the Plugin, we deliberately output to the console.
 		'no-console': 'off',
+		// We use a blocking confirm() dialog in the Plugin's Setup Wizard.
 		'no-alert': 'off',
 	},
 };
