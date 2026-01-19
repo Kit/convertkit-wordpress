008687a55ece1b532ed983d5f406e6c45e5baf70
Tim Carr <tim@n7studios.co.uk>
2026-01-19 16:29:15 +0800

    Move new functions to end of class

--- admin/importers/class-convertkit-admin-importer.php
+++ admin/importers/class-convertkit-admin-importer.php
@@ -42,6 +42,15 @@ abstract class ConvertKit_Admin_Importer {
 	public $block_name = '';
 
 	/**
+	 * Holds the ID attribute name for the third party Form plugin.
+	 *
+	 * @since   3.1.6
+	 *
+	 * @var     string
+	 */
+	public $block_id_attribute = '';
+
+	/**
 	 * Returns an array of third party form IDs and titles.
 	 *
 	 * @since   3.1.0
@@ -51,7 +60,7 @@ abstract class ConvertKit_Admin_Importer {
 	abstract public function get_forms();
 
 	/**
-	 * Returns an array of post IDs that contain the third party form shortcode or block.
+	 * Returns an array of post IDs that contain the third partyform shortcode.
 	 *
 	 * @since   3.1.5
 	 *
@@ -61,7 +70,7 @@ abstract class ConvertKit_Admin_Importer {
 
 		global $wpdb;
 
-		// Search post_content for the third party form shortcode or block and return array of post IDs.
+		// Search post_content for the third party form shortcode and return array of post IDs.
 		$results = $wpdb->get_col(
 			$wpdb->prepare(
 				"
@@ -110,6 +119,75 @@ abstract class ConvertKit_Admin_Importer {
 	}
 
 	/**
+	 * Replaces the third party form shortcode with the Kit form shortcode.
+	 *
+	 * @since   3.1.0
+	 *
+	 * @param   int $third_party_form_id    The ID of the third party form.
+	 * @param   int $form_id                The ID of the Kit form.
+	 */
+	public function replace_shortcodes_in_posts( $third_party_form_id, $form_id ) {
+
+		// Get Posts that contain the third party Form Shortcode.
+		$posts = $this->get_forms_in_posts();
+
+		// Bail if no Posts contain the third party Form Shortcode.
+		if ( empty( $posts ) ) {
+			return;
+		}
+
+		// Iterate through Posts and replace the third party Form Shortcode with the Kit Form Shortcode.
+		foreach ( $posts as $post_id ) {
+			// Get Post content.
+			$post_content = get_post_field( 'post_content', $post_id );
+
+			// Replace the third party Form Shortcode with the Kit Form Shortcode.
+			$post_content = $this->replace_shortcodes_in_content( $post_content, $third_party_form_id, $form_id );
+
+			// Update the Post content.
+			wp_update_post(
+				array(
+					'ID'           => $post_id,
+					'post_content' => $post_content,
+				),
+				false,
+				false // Don't fire after action hooks.
+			);
+
+		}
+
+	}
+
+	/**
+	 * Replaces the third party form shortcode with the Kit form shortcode in the given string.
+	 *
+	 * @since   3.1.0
+	 *
+	 * @param   string $content             Content containing third party Form Shortcodes.
+	 * @param   int    $third_party_form_id    Third Party Form ID.
+	 * @param   int    $form_id                Kit Form ID.
+
+	 * @return  string
+	 */
+	public function replace_shortcodes_in_content( $content, $third_party_form_id, $form_id ) {
+
+		$pattern = '/\['                                     // Start regex with an opening square bracket.
+			. preg_quote( $this->shortcode_name, '/' )       // Match the shortcode name, escaping any regex special chars.
+			. '[^\]]*?'                                      // Match any characters that are not a closing square bracket, non-greedy.
+			. '\b' . preg_quote( $this->shortcode_id_attribute, '/' ) // Match the id attribute word boundary and escape as needed.
+			. '\s*=\s*'                                      // Match optional whitespace around an equals sign.
+			. '(?:"' . preg_quote( (string) $third_party_form_id, '/' ) . '"|' . preg_quote( (string) $third_party_form_id, '/' ) . ')' // Match the form ID, quoted or unquoted.
+			. '[^\]]*?\]/i';                                 // Match any other characters (non-greedy) up to the closing square bracket, case-insensitive.
+
+		return preg_replace(
+			$pattern,
+			'[convertkit_form id="' . $form_id . '"]',
+			$content
+		);
+
+	}
+
+	/**
 	 * Returns an array of all unique form IDs from the posts that contain the third party form shortcode.
 	 *
 	 * @since   3.1.5
@@ -140,11 +218,11 @@ abstract class ConvertKit_Admin_Importer {
 	}
 
 	/**
-	 * Returns an array of form IDs within the third party form shortcode.
+	 * Returns an array of form IDs within the shortcode for the third party Form plugin.
 	 *
 	 * @since   3.1.5
 	 *
-	 * @param   string $content             Content containing third party form shortcode.
+	 * @param   string $content             Content containing third party Form Shortcodes.
 	 * @return  array
 	 */
 	public function get_form_ids_from_content( $content ) {
@@ -306,75 +384,6 @@ abstract class ConvertKit_Admin_Importer {
 		}
 
 		return $blocks;
-
-	}
-
-	/**
-	 * Replaces the third party form shortcode with the Kit form shortcode.
-	 *
-	 * @since   3.1.0
-	 *
-	 * @param   int $third_party_form_id    The ID of the third party form.
-	 * @param   int $form_id                The ID of the Kit form.
-	 */
-	public function replace_shortcodes_in_posts( $third_party_form_id, $form_id ) {
-
-		// Get Posts that contain the third party Form Shortcode.
-		$posts = $this->get_forms_in_posts();
-
-		// Bail if no Posts contain the third party Form Shortcode.
-		if ( empty( $posts ) ) {
-			return;
-		}
-
-		// Iterate through Posts and replace the third party Form Shortcode with the Kit Form Shortcode.
-		foreach ( $posts as $post_id ) {
-			// Get Post content.
-			$post_content = get_post_field( 'post_content', $post_id );
-
-			// Replace the third party Form Shortcode with the Kit Form Shortcode.
-			$post_content = $this->replace_shortcodes_in_content( $post_content, $third_party_form_id, $form_id );
-
-			// Update the Post content.
-			wp_update_post(
-				array(
-					'ID'           => $post_id,
-					'post_content' => $post_content,
-				),
-				false,
-				false // Don't fire after action hooks.
-			);
-
-		}
-
-	}
-
-	/**
-	 * Replaces the third party form shortcode with the Kit form shortcode in the given string.
-	 *
-	 * @since   3.1.0
-	 *
-	 * @param   string $content             Content containing third party Form Shortcodes.
-	 * @param   int    $third_party_form_id    Third Party Form ID.
-	 * @param   int    $form_id                Kit Form ID.
-
-	 * @return  string
-	 */
-	public function replace_shortcodes_in_content( $content, $third_party_form_id, $form_id ) {
-
-		$pattern = '/\['                                     // Start regex with an opening square bracket.
-			. preg_quote( $this->shortcode_name, '/' )       // Match the shortcode name, escaping any regex special chars.
-			. '[^\]]*?'                                      // Match any characters that are not a closing square bracket, non-greedy.
-			. '\b' . preg_quote( $this->shortcode_id_attribute, '/' ) // Match the id attribute word boundary and escape as needed.
-			. '\s*=\s*'                                      // Match optional whitespace around an equals sign.
-			. '(?:"' . preg_quote( (string) $third_party_form_id, '/' ) . '"|' . preg_quote( (string) $third_party_form_id, '/' ) . ')' // Match the form ID, quoted or unquoted.
-			. '[^\]]*?\]/i';                                 // Match any other characters (non-greedy) up to the closing square bracket, case-insensitive.
-
-		return preg_replace(
-			$pattern,
-			'[convertkit_form id="' . $form_id . '"]',
-			$content
-		);
 
 	}
 
