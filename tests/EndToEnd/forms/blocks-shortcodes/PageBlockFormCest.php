cbb3066a50c65062e592310cdce4a1e4946ee539
Tim Carr <tim@n7studios.co.uk>
2026-02-05 12:33:54 +0800

    Set a value so Heartbeat script isn’t deregistered

--- tests/EndToEnd/forms/blocks-shortcodes/PageBlockFormCest.php
+++ tests/EndToEnd/forms/blocks-shortcodes/PageBlockFormCest.php
@@ -824,16 +824,18 @@ class PageBlockFormCest
 		$I->setupKitPlugin($I);
 		$I->setupKitPluginResources($I);
 
-		// Disable Siteground Speed Optimizer's Heartbeat.
-		$I->haveOptionInDatabase('siteground_optimizer_heartbeat_post_interval', false );
-		$I->haveOptionInDatabase('siteground_optimizer_heartbeat_dashboard_interval', false );
-		$I->haveOptionInDatabase('siteground_optimizer_heartbeat_frontend_interval', false );
+				// Configure Siteground Speed Optimizer's Heartbeat.
+				$I->haveOptionInDatabase('siteground_optimizer_heartbeat_post_interval', 120 );
+				$I->haveOptionInDatabase('siteground_optimizer_heartbeat_dashboard_interval', 120 );
+				$I->haveOptionInDatabase('siteground_optimizer_heartbeat_frontend_interval', 120 );
 
 		// Activate Siteground Speed Optimizer Plugin.
 		$I->activateThirdPartyPlugin($I, 'sg-cachepress');
 
 		// Enable Siteground Speed Optimizer's "Combine JavaScript Files" setting.
 		$I->haveOptionInDatabase('siteground_optimizer_combine_javascript', '1');
+
+
 
 		// Add a Page using the Gutenberg editor.
 		$I->addGutenbergPage(
