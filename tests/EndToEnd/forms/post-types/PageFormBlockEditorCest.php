04ccb626b8003b495a1b311e241ba060d99baf3a
Tim Carr <tim@n7studios.co.uk>
2026-03-11 16:53:21 +0800

    Create new cest for block editor pages

--- tests/EndToEnd/forms/post-types/PageFormBlockEditorCest.php
+++ tests/EndToEnd/forms/post-types/PageFormBlockEditorCest.php
@@ -5,16 +5,16 @@ namespace Tests\EndToEnd;
 use Tests\Support\EndToEndTester;
 
 /**
- * Tests for Kit Forms on WordPress Pages.
+ * Tests for Kit Forms on WordPress Pages when using the block editor.
  *
- * @since   1.9.6
+ * @since   3.2.2
  */
-class PageFormCest
+class PageFormBlockEditorCest
 {
 	/**
 	 * Run common actions before running the test functions in this class.
 	 *
-	 * @since   1.9.6
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -25,33 +25,11 @@ class PageFormCest
 	}
 
 	/**
-	 * Test that the Pages > Add New screen has expected a11y output, such as label[for].
-	 *
-	 * @since   1.9.7.6
-	 *
-	 * @param   EndToEndTester $I  Tester.
-	 */
-	public function testAccessibility(EndToEndTester $I)
-	{
-		// Setup Kit plugin.
-		$I->setupKitPlugin($I);
-		$I->setupKitPluginResources($I);
-
-		// Navigate to Post Type (e.g. Pages / Posts) > Add New.
-		$I->amOnAdminPage('post-new.php?post_type=page');
-
-		// Confirm that settings have label[for] attributes.
-		$I->seeInSource('<label for="wp-convertkit-form">');
-		$I->seeInSource('<label for="wp-convertkit-landing_page">');
-		$I->seeInSource('<label for="wp-convertkit-tag">');
-	}
-
-	/**
 	 * Test that the 'Default' option for the Default Form setting in the Plugin Settings works when
 	 * creating and viewing a new WordPress Page, and there is no Default Form specified in the Plugin
 	 * settings.
 	 *
-	 * @since   1.9.6
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -65,16 +43,6 @@ class PageFormCest
 		$I->addGutenbergPage(
 			$I,
 			title: 'Kit: Page: Form: Default: None'
-		);
-
-		// Check the order of the Form resources are alphabetical, with the Default and None options prepending the Forms.
-		$I->checkSelectFormOptionOrder(
-			$I,
-			selectElement: '#wp-convertkit-form',
-			prependOptions: [
-				'Default',
-				'None',
-			]
 		);
 
 		// Configure Plugin Sidebar's Form setting = Default.
@@ -94,7 +62,7 @@ class PageFormCest
 	 * Test that the Default Form specified in the Plugin Settings works when
 	 * creating and viewing a new WordPress Page.
 	 *
-	 * @since   1.9.6
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -129,7 +97,7 @@ class PageFormCest
 	 * creating and viewing a new WordPress Page, and its position is set
 	 * to after the Page content.
 	 *
-	 * @since   2.5.8
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -176,7 +144,7 @@ class PageFormCest
 	 * creating and viewing a new WordPress Page, and its position is set
 	 * to before and after the Page content.
 	 *
-	 * @since   2.5.9
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -222,7 +190,7 @@ class PageFormCest
 	 * creating and viewing a new WordPress Page, and its position is set
 	 * to after the 3rd paragraph.
 	 *
-	 * @since   2.6.2
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -276,7 +244,7 @@ class PageFormCest
 	 * result in a fatal error when creating and viewing a new WordPress Page, and its position is set
 	 * to after the 3rd paragraph.
 	 *
-	 * @since   2.6.8
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -325,7 +293,7 @@ class PageFormCest
 	 * creating and viewing a new WordPress Page, and its position is set
 	 * to after the 2nd <h2> element.
 	 *
-	 * @since   2.6.6
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -379,7 +347,7 @@ class PageFormCest
 	 * creating and viewing a new WordPress Page, and its position is set
 	 * to after the 2nd <img> element.
 	 *
-	 * @since   2.6.2
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -433,7 +401,7 @@ class PageFormCest
 	 * creating and viewing a new WordPress Page, and its position is set
 	 * to a number greater than the number of elements in the content.
 	 *
-	 * @since   2.6.2
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -485,7 +453,7 @@ class PageFormCest
 	 * Test that the Default Legacy Form specified in the Plugin Settings works when
 	 * creating and viewing a new WordPress Page.
 	 *
-	 * @since   1.9.6.3
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -528,7 +496,7 @@ class PageFormCest
 	 * Test that 'None' Form specified in the Page Settings works when
 	 * creating and viewing a new WordPress Page.
 	 *
-	 * @since   1.9.6
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -561,7 +529,7 @@ class PageFormCest
 	 * Test that the Form specified in the Page Settings works when
 	 * creating and viewing a new WordPress Page.
 	 *
-	 * @since   1.9.6
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -595,7 +563,7 @@ class PageFormCest
 	 * Test that the Modal Form is output once when the Autoptimize Plugin is active and
 	 * its "Defer JavaScript" setting is enabled.
 	 *
-	 * @since   2.4.9
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -636,7 +604,7 @@ class PageFormCest
 	 * Test that the Modal Form is output once when the Debloat Plugin is active and
 	 * its "Defer JavaScript" and "Delay All Scripts" settings are enabled.
 	 *
-	 * @since   2.8.6
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -682,7 +650,7 @@ class PageFormCest
 	 * Test that the Modal Form is output once when the Jetpack Boost Plugin is active and
 	 * its "Defer Non-Essential JavaScript" setting is enabled.
 	 *
-	 * @since   2.4.5
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -729,7 +697,7 @@ class PageFormCest
 	 * Test that the Modal Form is output once when the LiteSpeed Cache Plugin is active and
 	 * its "Load JS Deferred" setting is enabled.
 	 *
-	 * @since   2.4.5
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -821,7 +789,7 @@ class PageFormCest
 	 * Test that the Modal Form is output once when the Perfmatters Plugin is active and its "Delay JavaScript"
 	 * setting is enabled.
 	 *
-	 * @since   2.4.7
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -873,7 +841,7 @@ class PageFormCest
 	 * Test that the Modal Form is output once when the WP Rocket Plugin is active and its "Delay JavaScript execution"
 	 * setting is enabled.
 	 *
-	 * @since   2.4.7
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -918,7 +886,7 @@ class PageFormCest
 	 * Test that the Legacy Form specified in the Page Settings works when
 	 * creating and viewing a new WordPress Page.
 	 *
-	 * @since   1.9.6.3
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -966,7 +934,7 @@ class PageFormCest
 	 * - the form belongs to another Kit account (i.e. API credentials were changed in the Plugin, but this Page's specified Form was not changed)
 	 * - the form was deleted from the Kit account.
 	 *
-	 * @since   1.9.7.2
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
@@ -1007,294 +975,11 @@ class PageFormCest
 	}
 
 	/**
-	 * Test that the Default Form for Pages displays when the Default option is chosen via
-	 * WordPress' Quick Edit functionality.
-	 *
-	 * @since   1.9.8.0
-	 *
-	 * @param   EndToEndTester $I  Tester.
-	 */
-	public function testQuickEditUsingDefaultForm(EndToEndTester $I)
-	{
-		// Setup Kit plugin.
-		$I->setupKitPlugin($I);
-		$I->setupKitPluginResources($I);
-
-		// Programmatically create a Page.
-		$pageID = $I->havePostInDatabase(
-			[
-				'post_type'  => 'page',
-				'post_title' => 'Kit: Page: Form: Default: Quick Edit',
-			]
-		);
-
-		// Quick Edit the Page in the Pages WP_List_Table.
-		$I->quickEdit(
-			$I,
-			postType: 'page',
-			postID: $pageID,
-			configuration: [
-				'form' => [ 'select', 'Default' ],
-			]
-		);
-
-		// Load the Page on the frontend site.
-		$I->amOnPage('/?p=' . $pageID);
-
-		// Check that no PHP warnings or notices were output.
-		$I->checkNoWarningsAndNoticesOnScreen($I);
-
-		// Confirm that one Kit Form is output in the DOM.
-		// This confirms that there is only one script on the page for this form, which renders the form.
-		$I->seeFormOutput($I, $_ENV['CONVERTKIT_API_FORM_ID']);
-	}
-
-	/**
-	 * Test that the defined form displays when chosen via
-	 * WordPress' Quick Edit functionality.
-	 *
-	 * @since   1.9.8.0
-	 *
-	 * @param   EndToEndTester $I  Tester.
-	 */
-	public function testQuickEditUsingDefinedForm(EndToEndTester $I)
-	{
-		// Setup Kit plugin.
-		$I->setupKitPlugin($I);
-		$I->setupKitPluginResources($I);
-
-		// Programmatically create a Page.
-		$pageID = $I->havePostInDatabase(
-			[
-				'post_type'  => 'page',
-				'post_title' => 'Kit: Page: Form: ' . $_ENV['CONVERTKIT_API_FORM_NAME'] . ': Quick Edit',
-			]
-		);
-
-		// Quick Edit the Page in the Pages WP_List_Table.
-		$I->quickEdit(
-			$I,
-			postType: 'page',
-			postID: $pageID,
-			configuration: [
-				'form' => [ 'select', $_ENV['CONVERTKIT_API_FORM_NAME'] ],
-			]
-		);
-
-		// Load the Page on the frontend site.
-		$I->amOnPage('/?p=' . $pageID);
-
-		// Check that no PHP warnings or notices were output.
-		$I->checkNoWarningsAndNoticesOnScreen($I);
-
-		// Confirm that one Kit Form is output in the DOM.
-		// This confirms that there is only one script on the page for this form, which renders the form.
-		$I->seeFormOutput($I, $_ENV['CONVERTKIT_API_FORM_ID']);
-	}
-
-	/**
-	 * Test that the Default Form for Pages displays when the Default option is chosen via
-	 * WordPress' Bulk Edit functionality.
-	 *
-	 * @since   1.9.8.0
-	 *
-	 * @param   EndToEndTester $I  Tester.
-	 */
-	public function testBulkEditUsingDefaultForm(EndToEndTester $I)
-	{
-		// Setup Kit plugin.
-		$I->setupKitPlugin($I);
-		$I->setupKitPluginResources($I);
-
-		// Programmatically create two Pages.
-		$pageIDs = array(
-			$I->havePostInDatabase(
-				[
-					'post_type'  => 'page',
-					'post_title' => 'Kit: Page: Form: Default: Bulk Edit #1',
-				]
-			),
-			$I->havePostInDatabase(
-				[
-					'post_type'  => 'page',
-					'post_title' => 'Kit: Page: Form: Default: Bulk Edit #2',
-				]
-			),
-		);
-
-		// Bulk Edit the Pages in the Pages WP_List_Table.
-		$I->bulkEdit(
-			$I,
-			postType: 'page',
-			postIDs: $pageIDs,
-			configuration: [
-				'form' => [ 'select', 'Default' ],
-			]
-		);
-
-		// Iterate through Pages to run frontend tests.
-		foreach ($pageIDs as $pageID) {
-			// Load Page on the frontend site.
-			$I->amOnPage('/?p=' . $pageID);
-
-			// Check that no PHP warnings or notices were output.
-			$I->checkNoWarningsAndNoticesOnScreen($I);
-
-			// Confirm that one Kit Form is output in the DOM.
-			// This confirms that there is only one script on the page for this form, which renders the form.
-			$I->seeFormOutput($I, $_ENV['CONVERTKIT_API_FORM_ID']);
-		}
-	}
-
-	/**
-	 * Test that the defined form displays when chosen via
-	 * WordPress' Bulk Edit functionality.
-	 *
-	 * @since   1.9.8.0
-	 *
-	 * @param   EndToEndTester $I  Tester.
-	 */
-	public function testBulkEditUsingDefinedForm(EndToEndTester $I)
-	{
-		// Setup Kit plugin.
-		$I->setupKitPlugin($I);
-		$I->setupKitPluginResources($I);
-
-		// Programmatically create two Pages.
-		$pageIDs = array(
-			$I->havePostInDatabase(
-				[
-					'post_type'  => 'page',
-					'post_title' => 'Kit: Page: Form: ' . $_ENV['CONVERTKIT_API_FORM_NAME'] . ': Bulk Edit #1',
-				]
-			),
-			$I->havePostInDatabase(
-				[
-					'post_type'  => 'page',
-					'post_title' => 'Kit: Page: Form: ' . $_ENV['CONVERTKIT_API_FORM_NAME'] . ': Bulk Edit #2',
-				]
-			),
-		);
-
-		// Bulk Edit the Pages in the Pages WP_List_Table.
-		$I->bulkEdit(
-			$I,
-			postType: 'page',
-			postIDs: $pageIDs,
-			configuration: [
-				'form' => [ 'select', $_ENV['CONVERTKIT_API_FORM_NAME'] ],
-			]
-		);
-
-		// Iterate through Pages to run frontend tests.
-		foreach ($pageIDs as $pageID) {
-			// Load Page on the frontend site.
-			$I->amOnPage('/?p=' . $pageID);
-
-			// Check that no PHP warnings or notices were output.
-			$I->checkNoWarningsAndNoticesOnScreen($I);
-
-			// Confirm that one Kit Form is output in the DOM.
-			// This confirms that there is only one script on the page for this form, which renders the form.
-			$I->seeFormOutput($I, $_ENV['CONVERTKIT_API_FORM_ID']);
-		}
-	}
-
-	/**
-	 * Test that the existing settings are honored and not changed
-	 * when the Bulk Edit options are set to 'No Change'.
-	 *
-	 * @since   1.9.8.0
-	 *
-	 * @param   EndToEndTester $I  Tester.
-	 */
-	public function testBulkEditWithNoChanges(EndToEndTester $I)
-	{
-		// Setup Kit plugin.
-		$I->setupKitPlugin($I);
-		$I->setupKitPluginResources($I);
-
-		// Programmatically create two Pages with a defined form.
-		$pageIDs = array(
-			$I->havePostInDatabase(
-				[
-					'post_type'  => 'page',
-					'post_title' => 'Kit: Page: Form: ' . $_ENV['CONVERTKIT_API_FORM_NAME'] . ': Bulk Edit with No Change #1',
-					'meta_input' => [
-						'_wp_convertkit_post_meta' => [
-							'form'         => $_ENV['CONVERTKIT_API_FORM_ID'],
-							'landing_page' => '',
-							'tag'          => '',
-						],
-					],
-				]
-			),
-			$I->havePostInDatabase(
-				[
-					'post_type'  => 'page',
-					'post_title' => 'Kit: Page: Form: ' . $_ENV['CONVERTKIT_API_FORM_NAME'] . ': Bulk Edit with No Change #2',
-					'meta_input' => [
-						'_wp_convertkit_post_meta' => [
-							'form'         => $_ENV['CONVERTKIT_API_FORM_ID'],
-							'landing_page' => '',
-							'tag'          => '',
-						],
-					],
-				]
-			),
-		);
-
-		// Bulk Edit the Pages in the Pages WP_List_Table.
-		$I->bulkEdit(
-			$I,
-			postType: 'page',
-			postIDs: $pageIDs,
-			configuration: [
-				'form' => [ 'select', '— No Change —' ],
-			]
-		);
-
-		// Iterate through Pages to run frontend tests.
-		foreach ($pageIDs as $pageID) {
-			// Load Page on the frontend site.
-			$I->amOnPage('/?p=' . $pageID);
-
-			// Check that no PHP warnings or notices were output.
-			$I->checkNoWarningsAndNoticesOnScreen($I);
-
-			// Confirm that one Kit Form is output in the DOM.
-			// This confirms that there is only one script on the page for this form, which renders the form.
-			$I->seeFormOutput($I, $_ENV['CONVERTKIT_API_FORM_ID']);
-		}
-	}
-
-	/**
-	 * Test that the Bulk Edit fields do not display when a search on a WP_List_Table
-	 * returns no results.
-	 *
-	 * @since   1.9.8.1
-	 *
-	 * @param   EndToEndTester $I  Tester.
-	 */
-	public function testBulkEditFieldsHiddenWhenNoPagesFound(EndToEndTester $I)
-	{
-		// Setup Kit plugin.
-		$I->setupKitPlugin($I);
-		$I->setupKitPluginResources($I);
-
-		// Emulate the user searching for Pages with a query string that yields no results.
-		$I->amOnAdminPage('edit.php?post_type=page&s=nothing');
-
-		// Confirm that the Bulk Edit fields do not display.
-		$I->dontSeeElement('#convertkit-bulk-edit');
-	}
-
-	/**
 	 * Deactivate and reset Plugin(s) after each test, if the test passes.
 	 * We don't use _after, as this would provide a screenshot of the Plugin
 	 * deactivation and not the true test error.
 	 *
-	 * @since   1.9.6.7
+	 * @since   3.2.2
 	 *
 	 * @param   EndToEndTester $I  Tester.
 	 */
