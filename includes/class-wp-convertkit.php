124f4a6c7133015eec4bf5eba01fe044117066c6
Tim Carr <tim@n7studios.co.uk>
2026-08-18 14:25:34 +0800

    Abilities API: Update MCP Adapter to 0.6.1

--- includes/class-wp-convertkit.php
+++ includes/class-wp-convertkit.php
@@ -245,15 +245,23 @@ class WP_ConvertKit {
 			return;
 		}
 
-		// Bail if the WordPress MCP Adapter autoloader is missing.
-		if ( ! file_exists( CONVERTKIT_PLUGIN_PATH . '/vendor/autoload.php' ) ) {
-			return;
+		// Load MCP Adapter if another Plugin hasn't already loaded it.
+		if ( ! class_exists( 'WP\\MCP\\Core\\McpAdapter' ) ) {
+			// Bail if the WordPress MCP Adapter autoloader is missing.
+			if ( ! file_exists( CONVERTKIT_PLUGIN_PATH . '/vendor/autoload.php' ) ) {
+				return;
+			}
+
+			// Load MCP Adapter.
+			require_once CONVERTKIT_PLUGIN_PATH . '/vendor/autoload.php';
+
+			// Bail if the MCP Adapter class doesn't exist - something went wrong with the autoloader.
+			if ( ! class_exists( 'WP\\MCP\\Core\\McpAdapter' ) ) {
+				return;
+			}
 		}
 
-		// Load MCP Adapter.
-		require_once CONVERTKIT_PLUGIN_PATH . '/vendor/autoload.php';
-
-		// Bail if the MCP Adapter class doesn't exist - something went wrong with the autoloader.
+		// Sanity check MCP Adapter is loaded.
 		if ( ! class_exists( 'WP\\MCP\\Core\\McpAdapter' ) ) {
 			return;
 		}
